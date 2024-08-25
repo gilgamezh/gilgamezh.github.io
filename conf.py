c75562e9fba6fb33f0b9d13ad29c8208bc0f5308
gilgamezh <mail@gilgamezh.me>
2024-08-25 09:06:00 +0200

    deploy to dropbox

--- conf.py
+++ conf.py
@@ -680,11 +680,12 @@ REDIRECTIONS = []
 # to `nikola deploy`.  If no arguments are specified, a preset
 # named `default` will be executed.  You can use as many presets
 # in a `nikola deploy` command as you like.
-# DEPLOY_COMMANDS = {
-#     'default': [
-#         "rsync -rav --delete --delete-after output/ joe@my.site:/srv/www/site",
-#     ]
-# }
+DEPLOY_COMMANDS = {
+    'default': [
+        'rdiff-backup output ~/Dropbox/blog/',
+
+    ]
+}
 
 # github_deploy configuration
 # For more details, read the manual:
